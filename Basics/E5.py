88afe8b0579d049b7f04b26671b32cfbb4183f2d
Ayman <aymanss2063@gmail.com>
2026-08-15 18:52:51 +0300

    Add age calculation and greeting in E5.py

---
+++ Basics/E5.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+current_year = int(input("Enter the current year: "))
+birth_year = int(input("Enter your birth year: "))
+
+age = current_year - birth_year
+
+print(f"Hello {name}, in {current_year} you are {age} years old!")
