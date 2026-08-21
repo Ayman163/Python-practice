6ad9ac55c9400543950d59825b66af7d9623ad00
Ayman <aymanss2063@gmail.com>
2026-08-21 19:07:17 +0300

    Refactor speed limit fine calculation logic

--- Mini Projects/P1, Smart Traffic Fine Calculator.py
+++ Mini Projects/P1, Smart Traffic Fine Calculator.py
@@ -3,11 +3,11 @@ driver_speed = int(input("Enter the driver speed: "))
 
 if driver_speed <= speed_limit:
     print("Speed is safe. Have a good trip!")
-elif driver_speed > speed_limit:
+else:
     over_limit = driver_speed - speed_limit
-    if over_limit > 0 and over_limit <= 20:
-        print("You are over the speed limit by", over_limit, "km/h.\nFine is $50")
-    elif over_limit > 20 and over_limit <= 40:
-        print("You are over the speed limit by", over_limit, "km/h.\nFine: $150")
-    elif over_limit > 40:
-        print("You are over the speed limit by", over_limit, "km/h.\nFine: $300 + License Suspended!")
+    if over_limit <= 20:
+        print(f"You are over the speed limit by {over_limit} km/h.\nFine is $50")
+    elif over_limit <= 40:
+        print(f"You are over the speed limit by {over_limit} km/h.\nFine: $150")
+    else:
+        print(f"You are over the speed limit by {over_limit} km/h.\nFine: $300 + License Suspended!")
