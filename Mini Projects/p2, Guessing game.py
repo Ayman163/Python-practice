5fd10f1abc81cbbee9182cf09af321fe08429e3c
Ayman <aymanss2063@gmail.com>
2026-08-23 22:12:34 +0300

    Implement number guessing game functionality
    Add guessing game logic with user input and feedback.

--- Mini Projects/p2, Guessing game.py
+++ Mini Projects/p2, Guessing game.py
@@ -1,18 +1,24 @@
 secret_number = 42
 count = 0
+
 while True:
     guess = input("Guess a number between 1 and 50: ")
     guess = int(guess)
+    
     if guess > secret_number:
         print("Too high! Try a lower number.")
         print(F"You have {4 - count} guesses left.")
+        
     elif guess < secret_number:
         print("Too low! Try a higher number.")
         print(F"You have {4 - count} guesses left.")
+        
     else:
         print("You guessed the secret number!")
         break
+        
     count += 1
+    
     if count == 5:
         print("You lose!")
         break
