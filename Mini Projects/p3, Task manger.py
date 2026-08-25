a264b32b0c01e1dd116338e204992fde8805d7e7
Ayman <aymanss2063@gmail.com>
2026-08-25 20:38:55 +0300

    Fix variable naming for user input in task manager

--- Mini Projects/p3, Task manger.py
+++ Mini Projects/p3, Task manger.py
@@ -8,18 +8,18 @@ while True :
     print("3-Number (3) to delete a task")
     print("4-Number (4) to exit")
     print("--------------------------------------------------------------------------------")
-    input = int(input("Enter your choice: "))
-    if input == 1:
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
         task.append(input("Enter the task name: "))
-    elif input == 2:
+    elif choice == 2:
         print("Task List")
         for i in range(len(task)):
             print(i+1,"-",task[i])
-    elif input == 3:
+    elif choice == 3:
         print("Enter the task number to delete: ")
         input = int(input("Enter your choice: "))
         del task[input-1]
-    elif input == 4:
+    elif choice == 4:
         print("Thank you for using the task manager")
         break
     else:
