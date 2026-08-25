e4db1cfd2d3f03bae80adff814e16149c28f05c2
Ayman <aymanss2063@gmail.com>
2026-08-25 20:36:52 +0300

    Add basic task manager functionality in Python

---
+++ Mini Projects/p3, Task manger.py
@@ -0,0 +1,26 @@
+while True :
+    task = []
+    print("--------------------------------------------------------------------------------")
+    print("Welcome to the task manager")
+    print("Enter your choice")
+    print("1-Number (1) to add a task")
+    print("2-Number (2) to show all tasks")
+    print("3-Number (3) to delete a task")
+    print("4-Number (4) to exit")
+    print("--------------------------------------------------------------------------------")
+    input = int(input("Enter your choice: "))
+    if input == 1:
+        task.append(input("Enter the task name: "))
+    elif input == 2:
+        print("Task List")
+        for i in range(len(task)):
+            print(i+1,"-",task[i])
+    elif input == 3:
+        print("Enter the task number to delete: ")
+        input = int(input("Enter your choice: "))
+        del task[input-1]
+    elif input == 4:
+        print("Thank you for using the task manager")
+        break
+    else:
+        print("Invalid choice")
