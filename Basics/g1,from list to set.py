a4eeb82af33071595fc596446f3b6f8d56b48af6
Ayman <aymanss2063@gmail.com>
2026-08-27 23:38:00 +0300

    Fix formatting and add motorcycle to unique_labels

--- Basics/g1,from list to set.py
+++ Basics/g1,from list to set.py
@@ -1,11 +1,9 @@
 #raw is list 
 raw_labels = ["car", "truck", "car", "bus", "person", "bus"]
 #unqiue it's set
-
 unique_labels = set(raw_labels)
 
 print(unique_labels)  # {'car', 'truck', 'bus', 'person'}
 
 #in set we use .add() not .append()
-
 unique_labels.add("motorcycle")
