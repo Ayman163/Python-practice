137f6d31cf61688842fdce4066219f848f71b9a0
Ayman <aymanss2063@gmail.com>
2026-08-31 13:46:45 +0300

    Refactor analyze_numbers function for clarity

--- Basics/g9.py
+++ Basics/g9.py
@@ -1,11 +1,10 @@
 def analyze_numbers(numbers: list):
     if not numbers:
         return None
-    elif numbers:
-        min_val = min(numbers)
-        max_val = max(numbers)
-        avg_val = sum(numbers) / len(numbers)
+    min_val = min(numbers)
+    max_val = max(numbers)
+    avg_val = sum(numbers) / len(numbers)
 
-        return {"min": min_val, "max": max_val, "average": avg_val}
+    return {"min": min_val, "max": max_val, "average": avg_val}
 
 print(analyze_numbers([1,2,3,4,5,6,7,8,77]))
