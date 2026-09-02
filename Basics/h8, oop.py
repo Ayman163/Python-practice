a763bb3ddb1fbabaf1f1ba6f17e615040ac3d13c
Ayman <aymanss2063@gmail.com>
2026-09-02 20:16:17 +0300

    Implement TrafficCamera class with video capture

---
+++ Basics/h8, oop.py
@@ -0,0 +1,13 @@
+class TrafficCamera:
+    def __init__(self, resolution, fps):
+        self.resolution = resolution
+        self.fps = fps
+        self.is_active = True
+
+    def capture_video(self):
+        if self.is_active:
+            print(f"The camera now captures video at {self.resolution} resolution and {self.fps} frame/second speed.")
+        else:
+            print("The camera is currently not working.")
+
+cam_1 = TrafficCamera("1080", 30)
