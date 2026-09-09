7719da98780aece639aa14b2035bcf28671e958b
Ayman <aymanss2063@gmail.com>
2026-09-09 15:31:57 +0300

    Add show_violations method call to radar_1

--- Mini Projects/Smart-Traffic-System/main.py
+++ Mini Projects/Smart-Traffic-System/main.py
@@ -4,5 +4,6 @@ radar_1 = SpeedCamera("4K", 60, 120)
 radar_1.check_speed(140)
 radar_1.capture_video()
 radar_1.check_multiple_speeds(110, 145, 95)
-
 radar_1.check_speed(145, color="Red", plate_number="XYZ-987", car_type="SUV")
+
+radar_1.show_violations()
