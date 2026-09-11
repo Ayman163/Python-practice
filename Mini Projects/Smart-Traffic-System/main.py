d17bb184d7ab2145e43514a1cb3fcfaeafa34de5
Ayman <aymanss2063@gmail.com>
2026-09-11 19:49:20 +0300

    Refactor main.py to implement main function
    Refactored main.py to include a main function that initializes the database and fetches traffic violations. Added functionality to check multiple speeds and display violations from the database.

--- Mini Projects/Smart-Traffic-System/main.py
+++ Mini Projects/Smart-Traffic-System/main.py
@@ -1,9 +1,21 @@
-from cameras import SpeedCamera 
+from database import init_db, get_violations
+from cameras import SpeedCamera
 
-radar_1 = SpeedCamera("4K", 60, 120)
-radar_1.check_speed(140)
-radar_1.capture_video()
-radar_1.check_multiple_speeds(110, 145, 95)
-radar_1.check_speed(145, color="Red", plate_number="XYZ-987", car_type="SUV")
+def main():
+    print("Starting Smart Traffic Management System...\n")
+    init_db()
 
-radar_1.show_violations()
+    radar = SpeedCamera(resolution="4K", fps=60, speed_limit=120)
+    radar.check_multiple_speeds(110, 145, 95, 160)
+    print("\n-------------------------------------------")
+
+    print("Fetching Traffic Violations from SQLite Database:")
+    violations = get_violations()
+
+    if violations:
+        for v in violations:
+            print(f" [Violation ID: {v[0]}]  Speed: {v[1]} km/h | Road Limit: {v[2]} km/h")
+    else:
+        print("No violations recorded.")
+if __name__ == "__main__":
+    main()
