f8c2c791a99a62c37965344ef758f16b61f94ab7
Ayman <aymanss2063@gmail.com>
2026-09-11 19:48:13 +0300

    Update database.py

--- Mini Projects/Smart-Traffic-System/database.py
+++ Mini Projects/Smart-Traffic-System/database.py
@@ -1,1 +1,15 @@
+import sqlite3 
 
+DB_NAME = "traffic_system.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS violations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    speed INTEGER,
+    speed_limit INTEGER
+    )""")
+    conn.commit()
+    conn.close()
