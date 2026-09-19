1e34a645a3e564958f889a4e543eabacb06b2d2a
Ayman <aymanss2063@gmail.com>
2026-09-19 13:00:07 +0300

    Add comments to tkinter window setup
    Added comments to clarify window creation and functionality.

---
+++ Libraries/tkinter/a1.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk() #Create a window 
+root.title("OB Timer") #Title of the window
+root.geometry("350x250") #Size of the window
+
+root.mainloop() #The engine let the window work all time 
