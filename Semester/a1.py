c6397aab31ccd7cd140eda441c885faddd3e707a
Ayman <aymanss2063@gmail.com>
2026-09-23 20:13:41 +0300

    Create a1.py

---
+++ Semester/a1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+idd = input("Enter your ID:")
+address = input("Enter your Address:")
+
+print("your name is:",name)
+print("Your ID is:",idd)
+print("Your address is:",address)
